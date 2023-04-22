475c24422b810a33a417e9ff8844f1375b95bfdf
s281182 <130214875+s281182@users.noreply.github.com>
2023-04-22 13:31:34 +0300

    Create variant4.py

---
+++ variant4.py
@@ -0,0 +1,11 @@
+def variant4():
+  ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные","погибшие"])
+  with open('titanic.csv') as file:
+      file_reader = csv.reader(file, delimiter=",")
+      for line in file_reader:
+          if ch == "спасенные":
+              if line[9] == '0' and line[1] == '1':
+                  st.success("Имя: "+line[3]+", пол: "+line[4]+", возраст: "+line[5])
+          else:
+              if line[9] == '0' and line[1] == '0':
+                  st.error("Имя: "+line[3]+", пол: "+line[4]+", возраст: "+line[5])
