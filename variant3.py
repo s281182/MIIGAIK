24aaea15126cd71b3140de86e969a4bd18b417f0
s281182 <130214875+s281182@users.noreply.github.com>
2023-04-22 16:07:39 +0300

    Update variant3.py

--- variant3.py
+++ variant3.py
@@ -3,6 +3,6 @@ def variant3():
     name=st.text_input("Введите имя пассажира")
     with open("data.csv") as file:
         for line in file.readlines()[1:]:
-        lst = line.rstrip().split(",")
-        if lst[1] == "1" and str(lst[3]+' '+lst[4])[1:-1].startswith(name):
-            st.success(lst[2]+', '+lst[3]+' '+lst[4]+', '+lst[6])
+            lst = line.rstrip().split(",")
+            if lst[1] == "1" and str(lst[3]+' '+lst[4])[1:-1].startswith(name):
+                st.success(lst[2]+', '+lst[3]+' '+lst[4]+', '+lst[6])
