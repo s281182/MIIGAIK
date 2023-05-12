e8660cdd1b6dea052a0dcf095a709d59c2bef022
s281182 <130214875+s281182@users.noreply.github.com>
2023-05-12 21:59:35 +0300

    Update variant4.py

--- variant4.py
+++ variant4.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import csv
+
+
+def pas_list(data,ch):
+    for line in data:
+        if ch == "спасенные":
+            if line[9] == '0' and line[1] == '1':
+                st.success("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
+        else:
+            if line[9] == '0' and line[1] == '0':
+                st.error("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
+
+                
 def variant4():
-  ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные","погибшие"])
-  with open('data.csv') as file:
-      file_reader = csv.reader(file, delimiter=",")
-      for line in file_reader:
-          if ch == "спасенные":
-              if line[9] == '0' and line[1] == '1':
-                  st.success("Имя: "+line[3]+", пол: "+line[4]+", возраст: "+line[5])
-          else:
-              if line[9] == '0' and line[1] == '0':
-                  st.error("Имя: "+line[3]+", пол: "+line[4]+", возраст: "+line[5])
+    ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные","погибшие"])
+    with open('data.csv') as file:
+        file_reader = csv.reader(file, delimiter=",")
+    pas_list(file_reader,ch)
