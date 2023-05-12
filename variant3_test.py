aeee474df1d543dc3346aca984520665de93e90c
ELZIK1987 <130274922+ELZIK1987@users.noreply.github.com>
2023-05-12 16:33:44 +0300

    Create variant3_test.py

---
+++ variant3_test.py
@@ -0,0 +1,10 @@
+from main import *
+
+
+def test_pass_list_all():
+    data = ['9,1,3,"Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",female,27', '5,1,3,"Allen, Mr. William Henry",male,35', '25,1,3,"Palsson, Miss. Torborg Danira",female,8']
+    assert get_pass_list(data, '') == ['3, "Johnson  Mrs. Oscar W (Elisabeth Vilhelmina Berg)", 27', '3, "Allen  Mr. William Henry", 35', '3, "Palsson  Miss. Torborg Danira", 8']
+
+def test_pass_list_John():
+    data = ['9,1,3,"Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",female,27', '5,1,3,"Allen, Mr. William Henry",male,35', '25,1,3,"Palsson, Miss. Torborg Danira",female,8']
+    assert get_pass_list(data, 'John') == ['3, "Johnson  Mrs. Oscar W (Elisabeth Vilhelmina Berg)", 27']
