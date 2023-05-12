25d191ed63ee03c734c1a6915ab855088fe740c3
ELZIK1987 <130274922+ELZIK1987@users.noreply.github.com>
2023-05-12 16:23:45 +0300

    Update variant3.py

--- variant3.py
+++ variant3.py
@@ -1,8 +1,16 @@
 import streamlit as st
-def variant3():
-    name=st.text_input("Введите имя пассажира")
-    with open("data.csv") as file:
-        for line in file.readlines()[1:]:
+
+
+def get_pass_list(data, name):
+        out_list = []
+        for line in data:
             lst = line.rstrip().split(",")
-            if lst[1] == "1" and str(lst[3]+' '+lst[4])[1:-1].startswith(name):
-                st.success(lst[2]+', '+lst[3]+' '+lst[4]+', '+lst[6])
+            if lst[1] == "1" and str(lst[3] + ' ' + lst[4])[1:-1].startswith(name):
+                out_list += [lst[2]+', '+lst[3]+' '+lst[4]+', '+lst[6]]
+        return out_list
+
+def variant3():
+    with open("data.csv") as file:
+        data = file.readlines()
+    name = st.text_input("Введите имя спасенного пассажира:")
+    st.table({"Данные спасенного пассажира(класс билета, имя, возраст):": get_pass_list(data, name)})
