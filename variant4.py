7a36991fa2e32e981b48795e8a1b25e00b7c7d0e
s281182 <130214875+s281182@users.noreply.github.com>
2023-05-13 17:29:32 +0300

    Update variant4.py

--- variant4.py
+++ variant4.py
@@ -1,26 +1,26 @@
 import streamlit as st
 
 
-def pas_list(data,ch):
-    list=[]
+def pas_list(data, ch):
+    list_p = []
     for line in data:
         lst = line.rstrip().split(",")
         if ch == "спасенные":
             if lst[10] == '0' and lst[1] == '1':
-                list.append("Имя: " + lst[3] + " " + lst[4] + ", пол: " + lst[5] + ", возраст: " + lst[6])
+                list_p.append("Имя: " + lst[3] + " " + lst[4] + ", пол: " + lst[5] + ", возраст: " + lst[6])
         else:
             if lst[10] == '0' and lst[1] == '0':
-                list.append("Имя: " + lst[3] + " " + lst[4] + ", пол: " + lst[5] + ", возраст: " + lst[6])
-    return list
+                list_p.append("Имя: " + lst[3] + " " + lst[4] + ", пол: " + lst[5] + ", возраст: " + lst[6])
+    return list_p
 
 
 def variant4():
-    ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные","погибшие"])
+    ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные", "погибшие"])
     with open('data.csv') as file:
         data = file.readlines()[1:]
-    list=pas_list(data,ch)
+    list_p = pas_list(data, ch)
     st.subheader("Всего " + ch + " с билетами нулевой стоимости: " + str(len(list)))
-    for lt in list:
+    for lt in list_p:
         if ch == "спасенные":
             st.success(lt)
         else:
