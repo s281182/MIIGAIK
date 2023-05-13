56b835b62c5d9146a1f8b5e42d07c5765d20f8c5
crusader38 <130214749+crusader38@users.noreply.github.com>
2023-05-13 20:32:47 +0300

    Update variant1_test.py

--- variant1_test.py
+++ variant1_test.py
@@ -2,10 +2,10 @@ from variant1 import calculate_dataframe_stats
 
 # age < 30, > 60, empty age -- max alike
 input1 = [
-    # ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+    # ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin']
     ['10', '0', '3', 'Hansen, Mr. Claus Peter', 'male', '41', '2', '0', '350026', '14.1083', '', 'S'],
     ['11', '0', '2', 'Giles, Mr. Frederick Edward', 'male', '21', '1', '0', '28134', '11.5', '', 'S'],
-    ['12', '1', '1', 'Swift, Mrs. Frederick Joel (Margaret Welles Barron)', 'female', '48', '0', '0', '17466', '25.9292'],
+    ['12', '1', '1', 'Swift, Mrs. Frederick Joel (Margaret Welles Barron)', 'female', '48', '0', '0', '17466'],
     ['13', '0', '3', 'Sage, Miss. Dorothy Edith "Dolly"', 'female', '', '8', '2', 'CA. 2343', '69.55', '', 'S'],
     ['14', '0', '2', 'Gill, Mr. John William', 'male', '24', '0', '0', '233866', '13', '', 'S'],
     ['15', '1', '2', 'Bystrom, Mrs. (Karolina)', 'female', '42', '0', '0', '236852', '13', '', 'S'],
@@ -23,7 +23,7 @@ output2 = (0, 0, 0, 0)
 # age beetwin [30, 60] range
 input3 = [
     ['10', '0', '3', 'Hansen, Mr. Claus Peter', 'male', '41', '2', '0', '350026', '14.1083', '', 'S'],
-    ['12', '1', '1', 'Swift, Mrs. Frederick Joel (Margaret Welles Barron)', 'female', '48', '0', '0', '17466', '25.9292'],
+    ['12', '1', '1', 'Swift, Mrs. Frederick Joel (Margaret Welles Barron)', 'female', '48', '0', '0', '17466'],
     ['15', '1', '2', 'Bystrom, Mrs. (Karolina)', 'female', '42', '0', '0', '236852', '13', '', 'S'],
 ]
 
