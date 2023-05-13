a177a23075cc1eba497d1bc94e91975e97717f79
s281182 <130214875+s281182@users.noreply.github.com>
2023-05-13 09:36:55 +0300

    Update variant4.py

--- variant4.py
+++ variant4.py
@@ -3,17 +3,25 @@ import csv
 
 
 def pas_list(data,ch):
+    list=[]
     for line in data:
         if ch == "спасенные":
             if line[9] == '0' and line[1] == '1':
-                st.success("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
+                list.append("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
         else:
             if line[9] == '0' and line[1] == '0':
-                st.error("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
+                list.append("Имя: " + line[3] + ", пол: " + line[4] + ", возраст: " + line[5])
+    return list
 
-                
+
 def variant4():
     ch = st.radio("Поиск пассажиров с билетом нулевой стоимости:", ["спасенные","погибшие"])
     with open('data.csv') as file:
         file_reader = csv.reader(file, delimiter=",")
-        pas_list(file_reader,ch)
+        list=pas_list(file_reader,ch)
+    st.subheader("Всего " + ch + " с билетами нулевой стоимости: " + str(len(list)))
+    for lt in list:
+        if ch == "спасенные":
+            st.success(lt)
+        else:
+            st.error(lt)
